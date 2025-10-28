e7463a7d897fe0b95829876802dc05185f6b1c9d
ltw0781 <ltw0781@naver.com>
2025-10-28 20:37:31 +0900

    first commit

---
+++ carrer_server/src/main/java/com/platform/carrer_server/CarrerServerApplication.java
@@ -0,0 +1,13 @@
+package com.platform.carrer_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrerServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarrerServerApplication.class, args);
+	}
+
+}
---
+++ carrer_server/src/test/java/com/platform/carrer_server/CarrerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.carrer_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarrerServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
